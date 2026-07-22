1c2e87a51ddd5ab927c5caa25e7f24c597feb6b4
Maksym Shevchyk <maksim.shevchik@gmail.com>
2026-07-22 11:47:33 +0200

    Installing boxicons

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import styles from './css/Header.module.css'
+
+export default function Header(){
+    return(
+        <>
+         <section className={styles.headerSection}>
+            
+         </section>
+        </>
+    )
+}
